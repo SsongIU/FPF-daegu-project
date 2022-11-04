f927839dc5038139719ab220610ed49e4ac4915a
Inuk Song <71516845+SsongIU@users.noreply.github.com>
2022-11-05 03:43:39 +0900

    update

--- app.py
+++ app.py
@@ -1,17 +1,56 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+import prophet
 import keras
 
 MODEL_PATH = "./model.h5"
+DATA_PATH = "./data/data.csv"
+
+data = pd.read_csv(DATA_PATH)
+
+
+def prophet_model(data, params):
+    data_c = data.copy()
+    data_c = data_c.rename(columns={"values": "y", "date": "ds"})
+    m = prophet.Prophet(**params)
+    m.add_seasonality(name="monthly", period=30.5, fourier_order=3, prior_scale=0.01, mode="multiplicative")
+    m.fit(data_c)
+    return m
+
+
+# model = keras.models.load_model(MODEL_PATH)
 
 st.title("Floating Population Warning")
-# model = keras.models.load_model(MODEL_PATH)
 
 _predict_option = st.selectbox("Select predict term", ("Long term predict", "Short term predict"))
 
-
 if _predict_option == "Long term predict":
     st.header("Long term floating population predict using prophet")
+    period = int(st.number_input("예측 기간"))
+
+    params = {
+    "changepoint_prior_scale": 0.5,
+    "changepoint_range": 0.8,
+    "seasonality_prior_scale": 10,
+    "weekly_seasonality": True,
+    "yearly_seasonality": False,
+    "daily_seasonality": True,
+    "seasonality_mode": "multiplicative",
+    "holidays_prior_scale": 10,
+    "interval_width": 0.8,
+}
+    model_prophet = prophet_model(data, params)
+
+    future = model_prophet.make_future_dataframe(periods=period, freq='H')
+    forecast = model_prophet.predict(future)
+    # plot forecast
+    fig = model_prophet.plot(forecast)
+    a = prophet.plot.add_changepoints_to_plot(fig.gca(), model_prophet, forecast)
+    # plot components
+    fig2 = model_prophet.plot_components(forecast)
+    st.write(fig)
+    st.write(fig2)
 elif _predict_option == "Short term predict":
     st.header("Short term floating population predict using GRU")
 
@@ -23,7 +62,7 @@ elif _predict_option == "Short term predict":
     ago_2 = float(st.number_input("2시간전 인구수"))
     ago_1 = float(st.number_input("1시간전 인구수"))
 
-    def predict():
-        row = np.array([[ago_7, ago_6]])
+    # def predict():
+    #     row = np.array([[ago_7, ago_6]])
 
     
--- model/prophet_model.py
+++ model/prophet_model.py
@@ -3,29 +3,43 @@ import prophet
 import matplotlib.pyplot as plt
 
 
-def prophet_model(data: pd.DataFrame, col: str, params: dict, periods: int, dtype="int"):
-    """시계열 예측 결과값 반환해주는 모델
-    Args:
-        data: 데이터프레임
-        col: 피쳐 컬럼 이름
-        params: prophets 파라미터
-        periods: 예측 일수
+# def prophet_model(data: pd.DataFrame, col: str, params: dict, periods: int, dtype="int"):
+#     """시계열 예측 결과값 반환해주는 모델
+#     Args:
+#         data: 데이터프레임
+#         col: 피쳐 컬럼 이름
+#         params: prophets 파라미터
+#         periods: 예측 일수
 
-    Returns:
-        pd.DataFrame: 예측 피쳐 데이터프레임형태 반환
-    """
+#     Returns:
+#         pd.DataFrame: 예측 피쳐 데이터프레임형태 반환
+#     """
+#     data_c = data.copy()
+#     # rename cols
+#     data_c = data_c.rename(columns={col: "y", "date": "ds"})
+
+#     # add params
+#     m = prophet.Prophet(**params)
+#     # m.add_country_holidays(country_name="KOR")
+#     m.add_seasonality(name="monthly", period=30.5, fourier_order=3, prior_scale=0.01, mode="multiplicative")
+
+#     # fit model
+#     m.fit(data_c)
+
+#     return m
+
+
+def prophet_model(data, params):
     data_c = data.copy()
-    # rename cols
-    data_c = data_c.rename(columns={col: "y", "DATE": "ds"})
-
-    # add params
+    data_c = data_c.rename(columns={"values": "y", "date": "ds"})
     m = prophet.Prophet(**params)
-    # m.add_country_holidays(country_name="KOR")
     m.add_seasonality(name="monthly", period=30.5, fourier_order=3, prior_scale=0.01, mode="multiplicative")
-
-    # fit model
     m.fit(data_c)
 
+    return m
+
+
+def prophet_predict(m, periods: int, dtype="int"):
     # future data
     future = m.make_future_dataframe(periods=periods)
     forecast = m.predict(future)
